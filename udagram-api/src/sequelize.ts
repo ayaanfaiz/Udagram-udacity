0cb4e8bb541c2694eda8d5042d63accc702005f0
Ayaan Faiz <ayaanfaiz@users.noreply.github.com>
2021-10-03 02:38:13 +0530

    added db"

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,4 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
-
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize("postgres://postgres:postgres@database-1.comticzyniuw.us-east-1.rds.amazonaws.com:5432/postgres");
