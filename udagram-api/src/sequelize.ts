8853e11749e5ef5641e2960fc7edfd8f5cdd0f7b
Ayaan Faiz <ayaanfaiz@users.noreply.github.com>
2021-10-03 16:06:06 +0530

    removed db

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,4 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize("postgres://postgres:postgres@database-1.comticzyniuw.us-east-1.rds.amazonaws.com:5432/postgres");
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.port,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
